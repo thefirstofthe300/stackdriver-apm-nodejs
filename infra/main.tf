e34cafa6089bd664786feeefc7e50cdbbd0c6d0a
Danny Seymour <dannyseeless@gmail.com>
2019-10-08 13:31:59 -0700

    feat: initial commit

---
+++ infra/main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  project = var.project
+}
+
+
+resource "google_container_cluster" "primary" {
+  name               = "stackdriver-apm"
+  location           = "us-west1-a"
+  initial_node_count = 1
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
